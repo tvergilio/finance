11440d17985db3758bbc10de52b08efc0fec452c
thalita <t.vergilio@leedsbeckett.ac.uk>
2021-12-07 10:03:28 +0000

    Swapped database from H2 to external MariaDB.

---
+++ migrations.sql
@@ -0,0 +1,57 @@
+create schema finance;
+
+grant ALL PRIVILEGES on finance.* to `finance-spring-user`;
+
+create or replace table account
+(
+    id         bigint auto_increment
+        primary key,
+    student_id varchar(255) null
+);
+
+INSERT INTO finance.account (id, student_id) VALUES (1, 'c7465688');
+INSERT INTO finance.account (id, student_id) VALUES (2, 'c3608824');
+
+create or replace table finance_user
+(
+    id   bigint auto_increment
+        primary key,
+    name varchar(255) null,
+    role varchar(255) null
+);
+
+INSERT INTO finance.finance_user (id, name, role) VALUES (1, 'Walter White', 'teacher');
+INSERT INTO finance.finance_user (id, name, role) VALUES (2, 'Jesse Pinkman', 'admin');
+
+create or replace table invoice
+(
+    id         bigint auto_increment
+        primary key,
+    amount     double   null,
+    due_date   datetime null,
+    status     int      null,
+    type       int      null,
+    account_fk bigint   null,
+    constraint FK237udfnwpogi0olckbos14vma
+        foreign key (account_fk) references account (id)
+);
+
+INSERT INTO finance.invoice (id, amount, due_date, status, type, account_fk) VALUES (1, 350.4, '2022-01-06 09:29:04', 0, 1, 1);
+INSERT INTO finance.invoice (id, amount, due_date, status, type, account_fk) VALUES (2, 810.5, '2021-10-07 09:29:04', 1, 1, 2);
+INSERT INTO finance.invoice (id, amount, due_date, status, type, account_fk) VALUES (3, 37.85, '2021-11-07 09:29:04', 2, 0, 1);
+INSERT INTO finance.invoice (id, amount, due_date, status, type, account_fk) VALUES (4, 1000, '2021-03-07 09:29:04', 1, 1, 1);
+INSERT INTO finance.invoice (id, amount, due_date, status, type, account_fk) VALUES (5, 15.6, '2021-12-22 09:29:04', 0, 0, 2);
+
+create or replace table account_invoice_list
+(
+    account_id      bigint not null,
+    invoice_list_id bigint not null,
+    constraint UK_bk0qum00dfjw1le2hlvd2qxlj
+        unique (invoice_list_id),
+    constraint FK2md7wefo9fyru31lawts2qw9b
+        foreign key (invoice_list_id) references invoice (id),
+    constraint FKf6bf69pytpp25ewt2dysq1muu
+        foreign key (account_id) references account (id)
+);
+
+
--- src/main/java/uk/ac/leedsbeckett/finance/LoadDatabase.java
+++ src/main/java/uk/ac/leedsbeckett/finance/LoadDatabase.java
@@ -17,24 +17,24 @@ class LoadDatabase {
     @Bean
     CommandLineRunner initDatabase(UserRepository userRepository, InvoiceRepository invoiceRepository, AccountRepository accountRepository) {
         return args -> {
-            userRepository.save(new User("Walter White", "teacher"));
-            userRepository.save(new User("Jesse Pinkman", "admin"));
-            userRepository.findAll().forEach(user -> log.info("Preloaded " + user));
-
-            Account account1 = new Account("c7465688");
-            Account account2 = new Account("c3608824");
-
-            accountRepository.save(account1);
-            accountRepository.save(account2);
-
-            invoiceRepository.save(new Invoice(350.40, LocalDateTime.now().plusDays(30), Type.TUITION_FEES, Status.OUTSTANDING, account1));
-            invoiceRepository.save(new Invoice(810.50, LocalDateTime.now().minusMonths(2), Type.TUITION_FEES, Status.PAID, account2));
-            invoiceRepository.save(new Invoice(37.85, LocalDateTime.now().minusMonths(1), Type.LIBRARY_FINE, Status.CANCELLED, account1));
-            invoiceRepository.save(new Invoice(1000.0, LocalDateTime.now().minusMonths(9), Type.TUITION_FEES, Status.PAID, account1));
-            invoiceRepository.save(new Invoice(15.60, LocalDateTime.now().plusDays(15), Type.LIBRARY_FINE, Status.OUTSTANDING, account2));
-
-            invoiceRepository.findAll().forEach(invoice -> log.info("Preloaded " + invoice));
-            accountRepository.findAll().forEach(account -> log.info("Preloaded " + account));
+//            userRepository.save(new User("Walter White", "teacher"));
+//            userRepository.save(new User("Jesse Pinkman", "admin"));
+//            userRepository.findAll().forEach(user -> log.info("Preloaded " + user));
+//
+//            Account account1 = new Account("c7465688");
+//            Account account2 = new Account("c3608824");
+//
+//            accountRepository.save(account1);
+//            accountRepository.save(account2);
+//
+//            invoiceRepository.save(new Invoice(350.40, LocalDateTime.now().plusDays(30), Type.TUITION_FEES, Status.OUTSTANDING, account1));
+//            invoiceRepository.save(new Invoice(810.50, LocalDateTime.now().minusMonths(2), Type.TUITION_FEES, Status.PAID, account2));
+//            invoiceRepository.save(new Invoice(37.85, LocalDateTime.now().minusMonths(1), Type.LIBRARY_FINE, Status.CANCELLED, account1));
+//            invoiceRepository.save(new Invoice(1000.0, LocalDateTime.now().minusMonths(9), Type.TUITION_FEES, Status.PAID, account1));
+//            invoiceRepository.save(new Invoice(15.60, LocalDateTime.now().plusDays(15), Type.LIBRARY_FINE, Status.OUTSTANDING, account2));
+//
+//            invoiceRepository.findAll().forEach(invoice -> log.info("Preloaded " + invoice));
+//            accountRepository.findAll().forEach(account -> log.info("Preloaded " + account));
 
         };
     }
