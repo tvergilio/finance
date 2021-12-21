36f026c8035676378984919f9e68e6bc50254a6c
thalita <t.vergilio@leedsbeckett.ac.uk>
2021-12-21 14:41:14 +0000

    Fixed currency display issue.

--- src/main/java/uk/ac/leedsbeckett/finance/MiscellaneousBeans.java
+++ src/main/java/uk/ac/leedsbeckett/finance/MiscellaneousBeans.java
@@ -8,7 +8,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import uk.ac.leedsbeckett.finance.model.*;
+
+import java.util.Locale;
 
 @Configuration
 class MiscellaneousBeans {
@@ -30,6 +34,13 @@ class MiscellaneousBeans {
         messageSource.setBasename("classpath:messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.UK);
+        return sessionLocaleResolver;
     }
 
     @Bean
