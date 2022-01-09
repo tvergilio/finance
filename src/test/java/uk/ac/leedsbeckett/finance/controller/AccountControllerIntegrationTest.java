d16d292d4c0f36e23558b28ed15eaf72c19d4c26
thalita <t.vergilio@leedsbeckett.ac.uk>
2022-01-09 17:37:58 +0000

    Added generated REST documentation for account.

--- src/test/java/uk/ac/leedsbeckett/finance/controller/AccountControllerIntegrationTest.java
+++ src/test/java/uk/ac/leedsbeckett/finance/controller/AccountControllerIntegrationTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.hypermedia.LinkDescriptor;
+import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -16,12 +18,13 @@ import uk.ac.leedsbeckett.finance.model.AccountRepository;
 
 import java.util.List;
 
-import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
-import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -39,6 +42,9 @@ public class AccountControllerIntegrationTest {
     @Autowired
     private AccountRepository accountRepository;
 
+    private List<FieldDescriptor> accountResponseFieldDescriptor;
+    private List<LinkDescriptor> linkDescriptors;
+
     @BeforeEach
     public void setUp() {
         Account account1 = new Account("c6666666");
@@ -46,6 +52,7 @@ public class AccountControllerIntegrationTest {
         Account account2 = new Account("c9999999");
         account2.setId(2L);
         accountRepository.saveAll(List.of(account1, account2));
+        configureRestDocumentation();
     }
 
     @Test
@@ -57,20 +64,24 @@ public class AccountControllerIntegrationTest {
                         .contentTypeCompatibleWith(MediaTypes.HAL_JSON))
                 .andExpect(jsonPath("$.studentId").value("c6666666"))
                 .andExpect(jsonPath("$.id").value("1"))
-                .andDo(document("accounts", pathParameters(
-                        parameterWithName("id").description("The id of the account to return.")),
-                        links(linkWithRel("self").description("Link to this resource."),
-                                linkWithRel("accounts").description("Link to all accounts."))));
+
+                .andDo(document("get-account-by-id",
+                        pathParameters(parameterWithName("id").description("The id of the account to return.")),
+                        links(linkDescriptors),
+                        responseFields(accountResponseFieldDescriptor)));
     }
 
-//    @Test
-//    public void b_givenAccount_whenDelete_thenStatus204() throws Exception {
-//        mvc.perform(delete("/accounts/{id}", 3)
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isNoContent())
-//                .andDo(document("accounts", pathParameters(
-//                        parameterWithName("id").description("The id of the account to delete"))));
-//    }
+    @Test
+    public void b_givenAccount_whenDelete_thenStatus204() throws Exception {
+        mvc.perform(delete("/accounts/{id}", 3)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent())
+                .andDo(document("accounts", pathParameters(
+                        parameterWithName("id").description("The id of the account to delete"))))
+
+                .andDo(document("delete-account-by-id",
+                        pathParameters(parameterWithName("id").description("The id of the account to delete."))));
+    }
 
     @Test
     public void givenAccounts_whenGetAccounts_thenStatus200() throws Exception {
@@ -80,7 +91,13 @@ public class AccountControllerIntegrationTest {
                 .andExpect(content()
                         .contentTypeCompatibleWith(MediaTypes.HAL_JSON))
                 .andExpect(jsonPath("$._embedded.accountList[0].studentId").value("c6666666"))
-                .andExpect(jsonPath("$._embedded.accountList[1].studentId").value("c9999999"));
+                .andExpect(jsonPath("$._embedded.accountList[1].studentId").value("c9999999"))
+
+                .andDo(document("get-accounts",
+                        links(halLinks(), linkWithRel("self").description("Link to this resource.")),
+                        responseFields(fieldWithPath("_embedded.accountList[]").description("A list of accounts"),
+                                subsectionWithPath("_links").ignored())
+                                .andWithPrefix("_embedded.accountList[].", accountResponseFieldDescriptor)));
     }
 
     @Test
@@ -126,7 +143,12 @@ public class AccountControllerIntegrationTest {
                         .content("{\"studentId\": \"c3429928\"}"))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.studentId").value("c3429928"))
-                .andExpect(jsonPath("$.hasOutstandingBalance").value(false));
+                .andExpect(jsonPath("$.hasOutstandingBalance").value(false))
+
+                .andDo(document("create-account",
+                requestFields(fieldWithPath("studentId").description("The external student ID.")),
+                links(linkDescriptors),
+                responseFields(accountResponseFieldDescriptor)));
     }
 
     @Test
@@ -160,6 +182,17 @@ public class AccountControllerIntegrationTest {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound())
                 .andExpect(content().string("Could not find account 1000"));
+    }
+
+    private void configureRestDocumentation() {
+        accountResponseFieldDescriptor = List.of(
+                fieldWithPath("id").description("The internal ID of the account."),
+                fieldWithPath("studentId").description("The external student ID."),
+                fieldWithPath("hasOutstandingBalance").description("Whether the account has an outstanding balance"),
+                subsectionWithPath("_links").ignored()
+        );
+        linkDescriptors = List.of(linkWithRel("self").description("Link to this resource."),
+                linkWithRel("accounts").description("Link to all accounts."));
     }
 
     @AfterEach
